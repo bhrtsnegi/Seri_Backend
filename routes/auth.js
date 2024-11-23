1ccba23ba49b2d4a66cced6e4b24366f3fd27624
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-23 10:15:56 +0530

    add register api

---
+++ routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const router = express.Router();
+
+//Register User
+router.post('/signup', async(req, res)=>{
+    try{
+        const{username, email, name, password, dateOfBirth} = req.body;
+        const user = new User({
+            username,
+            email,
+            name,
+            password,
+            dateOfBirth
+        });
+        await user.save();
+        res.status(201).json({message: "User Registered Successfullly"});
+    } catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
