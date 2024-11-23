9a362e8453083eab1790dcaa646803e55b140e51
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-23 09:58:46 +0530

    add user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
