e0654c38b8298feebd8c610bc7ba8925d52b076e
Bharat Negi <1bhrtsnegi@gmail.com>
2024-11-23 09:43:29 +0530

    Bharat: Server Setup  (#1)
    * add basic server configuration
    
    * add dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb://localhost:27017/authDB')
+.then(()=>console.log('Connected to MongoDb'))
+.catch(()=>console.log("Database Connection Failed ", err)
+);
+
+//test route
+app.get('/', (req, res)=>{
+    res.send("Server is running");
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`));
