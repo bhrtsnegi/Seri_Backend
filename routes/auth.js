918df390fcf4c5f277a063274fa6f0c59e5d55a2
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-25 09:25:40 +0530

    add * login authentication using email and password * token initialization * cookie setting

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
 
 const router = express.Router();
 
@@ -8,11 +10,12 @@ const router = express.Router();
 router.post('/signup', async(req, res)=>{
     try{
         const{username, email, name, password, dateOfBirth} = req.body;
+        const hashedPassword = bcrypt.hashSync(password, 10);
         const user = new User({
             username,
             email,
             name,
-            password,
+            password: hashedPassword,
             dateOfBirth
         });
         await user.save();
@@ -21,5 +24,33 @@ router.post('/signup', async(req, res)=>{
         res.status(500).json({error: err.message});
     }
 });
+
+//login user
+router.post('/login', async(req, res)=>{
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) return res.status(404).json({message: "User Not Found"});
+        const validPassword = await bcrypt.compare(password, user.password);
+        if(!validPassword) return res.status(404).json({message: "Invalid Credentials"});
+
+        const token = jwt.sign(
+            {id: user._id},
+            process.env.JWT_SECRET
+        );
+
+        res.cookie("authToken", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV = 'production',
+        });
+
+        res.status(200).json({
+            message: "User logged in successfully"
+        });
+    }
+    catch(err){
+        res.status(500).json({error : err.message});
+    }
+})
 
 module.exports = router;
